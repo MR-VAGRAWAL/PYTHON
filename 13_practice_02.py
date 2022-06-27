a28f498c84b8ae8ea01040ff4fd58d77703c60de
MR-VAGRAWAL <vtibrewal2003@gmail.com>
2022-06-27 15:42:42 +0530

    Intial Learning

---
+++ 13_practice_02.py
@@ -0,0 +1,9 @@
+# Write a programme to display a user entered name followed by Good Afternoon using input() function. 
+
+name = input("Enter your name :- \n ")
+print("Good Afternoon , " +  name)
+
+# Write a programme to fill in a letter template given below with name and date.
+#        letter = ''' Dear <|NAME|>,
+#                             You are selected!
+#                             <|DATE|> '''
